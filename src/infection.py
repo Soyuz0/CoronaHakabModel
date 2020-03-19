cc621b6e22c281a94f860e2dfaee1a11f8feda3f
omri312 <omermaya@gmail.com>
2020-03-19 17:16:41 +0200

    Added all basic classes

---
+++ src/infection.py
@@ -0,0 +1,18 @@
+
+class InfectionManager:
+    """
+    Manages the "Infection" stage of the simulation.
+    """
+    
+    def __init__():
+        pass
+    
+class InfectionPolicy:
+    """
+    Holds relevant information about policys that effect infection generaly.
+    """
+    
+    def __init__():
+        pass
+    
+    
