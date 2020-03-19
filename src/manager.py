1f8b5c7814ece733398f70c8be476511aeab9eae
ben avrahami <dontcallme@illcall.you>
2020-03-19 19:10:50 +0200

    minimalist move implementation

--- src/manager.py
+++ src/manager.py
@@ -4,6 +4,8 @@ from itertools import chain
 from typing import Iterable, Callable
 
 from src.agent import Agent, Location
+from src.infection import InfectionManager
+from src.move import MoveManager
 
 
 class SimulationManager:
@@ -11,26 +13,28 @@ class SimulationManager:
     A simulation manager is the main class, it holds all the locations and agents in the
      simulation and oversees their coordination
     """
-    def __init__(self, agents: Iterable[Agent], locations: Iterable[Location]):
-        self.locations = list(locations)
-        self.agents = list(agents)
+
+    def __init__(self, location_generator, agent_generator, move_policy, infection_policy):
+        self.locations = location_generator()
+        self.agents = agent_generator(self.locations)
+
+        self.move_manager = MoveManager(self, move_policy)
+        self.infection_manager = None  # todo
 
         self.sim_time: float = None  # sim_time is measured in steps performed, and not in any real-time measurement
 
-        self.logger: Callable[[SimulationManager],None] = lambda x:None  # todo
+        self.logger: Callable[[SimulationManager], None] = lambda x: None  # todo
         self.to_continue: Callable[[SimulationManager], bool] = lambda x: False  # todo
 
     def step(self):
         self.sim_time += 1
         # move stage
-        dests = [(a, d) for a in self.agents if (d := a.next_location(self))]
-        for a,t in dests:
-            a.move_to(t)
+        self.move_manager.step()
 
         # infection stage
         for entity in chain(
-            self.agents,
-            self.locations
+                self.agents,
+                self.locations
         ):
             entity.time_passed(self)
 
--- src/move.py
+++ src/move.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from __future__ import annotations
 
 
 class MoveManager:
@@ -6,15 +6,20 @@ class MoveManager:
     Manages the "Move" stage of the simulation.
     """
 
+    def __init__(self, policy: MovePolicy):
+        self.owner = None
+        self.policy = policy
+
     def step(self):
-        pass
+        destinations = [(a, d) for a in self.owner.agents if (d := self.policy.get_next_location(a, self.owner))]
+        for a, t in destinations:
+            a.move_to(t)
 
 
-class MovePolicy(ABC):
+class MovePolicy:
     """
     Holds relevant information about policys that effect movement generaly.
     """
 
-    @abstractmethod
-    def get_next_location(self):
-        pass
+    def get_next_location(self, agent, manager):
+        return agent.next_location(manager)
