8b7f40858d6c0bf903c56b8e027e3f69d0579b66
Ido Rosenbaum <ro2363@gmail.com>
2020-03-20 16:33:35 +0200

    simulation manager

--- src/agent.py
+++ src/agent.py
@@ -1,52 +1,25 @@
-from __future__ import annotations
+from enum import IntEnum
 
-from abc import ABC, abstractmethod
-from typing import Optional
 
-class Agent(ABC):
+class Agent:
     """
-    Aan agent is an entity that can move between locations
-    its location is a specific circle that represents a physical location
+    This class represents a person in our doomed world.
     """
-    __slots__ = "initial_location", "health"
 
-    def __init__(self, initial_location: Location, agent_id):
-        super().__init__()
+    def __init__(self, ssn):
+        self.ssn = ssn
+        self.medical_state = MedicalState.Healthy
 
-        self.location = initial_location
-        initial_location.add(self)
+    def __str__(self):
+        return "<Person,  ssn={}, medical={}>".format(self.ssn, self.medical_state)
 
-        ## Agent Constant Values ##
-        self.agent_id = agent_id
 
-        ## Agent Updating Values ##
-        self.health = HealthMachine[0]
-        self.infection_rate = 0 # The amount in which the agent is infectious.
-
-    @abstractmethod
-    def next_location(self, manager) -> Optional[Location]:
-        """
-        Get the location the agent will move to within the next time step, or None if no movement will occur
-        """
-        pass
-
-    def move_to(self, destination: Location):
-        """
-        Change an agent's location
-        """
-        self.location.remove(self)
-        self.location = destination
-        destination.add(self)
-
-    def change_health(self, new_state):
-        """
-        Change the agent's health state
-        """
-        for c in self.circles:
-            if isinstance(c, Location):  # todo slow?
-                c.health_changed(self, new_state)
-        self.health = new_state
-
-    def time_passed(self, manager):
-        if n_health := self.health.next():
-            self.change_health(n_health)
+class MedicalState(IntEnum):
+    Immune = -1
+    Healthy = 0
+    Infected = 1
+    Asymptomatic = 2
+    SymptomaticLow = 3
+    SymptomaticMedium = 4
+    SymptomaticHigh = 5
+    Deceased = 6
--- src/manager.py
+++ src/manager.py
@@ -1,9 +1,55 @@
+from  affinity_matrix import AffinityMAtrix
+import logging
+from agent import Agent
+
 class SimulationManager:
     """
     A simulation manager is the main class, it manages the steps performed with policies
     """
+
+    # GENERAL SIMULATION CONSTS:
+    SIZE_OF_POPULATION = 1000
+    STEPS_TO_RUN = 100
+
     def __init__(self):
-        pass
+        self.matrix = AffinityMAtrix(self.SIZE_OF_POPULATION)
+        self.agents = [Agent(i) for i in xrange(self.SIZE_OF_POPULATION)]
+        self.logger = logging.getLogger('simulation')
+        self.logger.setLevel(logging.INFO)
+
+        self.logger.info("Created new simulation.")
 
     def step(self):
+        """
+        run one step
+
+        pseudo:
+        update matrix with current policy
+        v = [i for i in self.agents.is_sick]
+        perform w*v
+        for each person in v:
+            if rand() < v[i]
+                agents[i].infect
+
+        """
         pass
+
+    def run(self):
+        """
+        runs full simulation
+        """
+
+        for i in xrange(sm.STEPS_TO_RUN):
+            self.logger.info("performing step {}/{}".format(i, self.STEPS_TO_RUN))
+            self.step()
+
+    def __str__(self):
+        return "<SimulationManager: SIZE_OF_POPULATION={}, STEPS_TO_RUN={}>".format(self.SIZE_OF_POPULATION,
+                                                                                    self.STEPS_TO_RUN)
+
+
+if __name__ == '__main__':
+    sm = SimulationManager()
+    sm.run()
+
+
